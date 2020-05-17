3980e4a79db620bd25e4afd38d39d640dfc24876
Ramez <rameznoshy@gmail.com>
2020-05-18 00:33:40 +0200

    refactor: gray counter
    Co-authored-by: Sameh Amnoun <SamehAmnoun@gmail.com>

--- tests/tb_grayToBinary.vhd
+++ tests/tb_grayToBinary.vhd
@@ -1,62 +1,55 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
+
 ENTITY tb_grayToBinary IS
 END tb_grayToBinary;
- 
-ARCHITECTURE behavior OF tb_grayToBinary IS 
- 
-    COMPONENT grayToBinary
-    PORT(
-         clk : IN  std_logic;
-         rst : IN  std_logic;
-         en : IN  std_logic;
-         g : INOUT  std_logic_vector(3 downto 0);
-         b : OUT  std_logic_vector(3 downto 0)
-        );
-    END COMPONENT;
-    
 
-   signal clk : std_logic := '0';
-   signal rst : std_logic := '0';
-   signal en : std_logic := '0';
+ARCHITECTURE behavior OF tb_grayToBinary IS
 
-   signal g : std_logic_vector(3 downto 0);
+   COMPONENT grayToBinary
+      PORT (
+         gray_in : IN std_logic_vector(3 DOWNTO 0);
+         bin_out : OUT std_logic_vector(3 DOWNTO 0)
+      );
+   END COMPONENT;
+   SIGNAL gray_in : std_logic_vector(3 DOWNTO 0);
 
-   signal b : std_logic_vector(3 downto 0);
+   SIGNAL bin_out : std_logic_vector(3 DOWNTO 0);
+   SIGNAL clk : std_logic;
 
-   constant clk_period : time := 10 ns;
- 
+   CONSTANT clk_period : TIME := 10 ns;
+
 BEGIN
- 
-   uut: grayToBinary PORT MAP (
-          clk => clk,
-          rst => rst,
-          en => en,
-          g => g,
-          b => b
-        );
 
-   clk_process :process
-   begin
-        clk <= '0';
-        wait for clk_period/2;
-        clk <= '1';
-        wait for clk_period/2;
-   end process;
- 
+   uut : grayToBinary PORT MAP(
+      gray_in => gray_in,
+      bin_out => bin_out
+   );
 
-   stim_proc: process
-   begin        
-      rst <= '1';
-            WAIT UNTIL clk='1' AND clk'EVENT;
-            rst <= '0';
-            en <= '1';
-            FOR index IN 0 To 4 LOOP
-                  WAIT UNTIL clk='1' AND clk'EVENT;
-            END LOOP;
-            wait;
+   clk_process : PROCESS
+   BEGIN
+      clk <= '0';
+      WAIT FOR clk_period/2;
+      clk <= '1';
+      WAIT FOR clk_period/2;
+   END PROCESS;
 
-   end process;
+   stim_proc : PROCESS
+   BEGIN
+      gray_in <= "0001";
+      WAIT UNTIL clk = '1' AND clk'EVENT;
+      ASSERT bin_out <= "0001";
+      gray_in <= "0011";
+      WAIT UNTIL clk = '1' AND clk'EVENT;
+      ASSERT bin_out <= "0010";
+      gray_in <= "0111";
+      WAIT UNTIL clk = '1' AND clk'EVENT;
+      ASSERT bin_out <= "0100";
+      gray_in <= "1110";
+      WAIT UNTIL clk = '1' AND clk'EVENT;
+      ASSERT bin_out <= "1001";
+      WAIT;
+
+   END PROCESS;
 
 END;
